850d6bad205f23e60358e814fa666eb3cdc845eb
Sukshanj <sukshanj@gmail.com>
2022-10-25 20:33:56 +0530

    Added the initial submission form to index page before launching the  game

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("heading1").innerHTML = "PLAYER INFORMATION FORM TO BE FILLED BEFORE PLAYING THE GAME ";
+console.log("\n");
+
+
+function resetom() {
+
+	var ele= document.getElementsByName("Gender");
+	for(var i=0; i<ele.length; i++){
+		ele[i].checked= false;
+	}
+
+	event.preventDefault();
+}
