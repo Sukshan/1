9eb44811d5be4d9f020fcf605ec9540124a1e3b7
Sukshanj <sukshanj@gmail.com>
2022-10-30 09:10:07 +0530

    add script to convert player info into json objects and store in array after submit click event and also store it in local storage.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+// array of player of information
+let players= [];
+
+
+// funtion to add player after some event happens
+const addplayer=(ev)=>{
+	// console.log('yes add player funtion ran')
+	ev.preventDefault();
+	let player={
+		id: Date.now(),
+		fname: document.getElementById('fname').value,
+		lname: document.getElementById('lname').value,
+		gender: document.getElementsByName('Gender').value,
+		age: document.getElementById('age').value
+	}
+	// console.log('reached this point')
+	players.push(player);
+	document.getElementById('iniregform').reset();
+
+
+	//instead of adding print statement, lets add a standard message
+	console.warn('added', {players});
+
+
+	//saving to local storage
+	localStorage.setItem('PlayerList', JSON.stringify(players));
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+	document.getElementById('submit').addEventListener('click', addplayer);
+});
+
+
 
 
 function resetom() {
-
 	var ele= document.getElementsByName("Gender");
 	for(var i=0; i<ele.length; i++){
 		ele[i].checked= false;
@@ -27,8 +59,21 @@ const change_turn= ()=>{
 }
 
 //funtion to check whether some one won or not
-const checkwin =()=>{
 
+const checkwin= ()=>{
+	let wins=[
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	wins.forEach(e=>{
+		
+	})
 }
 
 //game logic
